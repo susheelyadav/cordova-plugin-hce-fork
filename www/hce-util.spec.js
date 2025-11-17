e7fc3b650f041634af4d8f2d58bf61dab51c0530
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2025-11-17 21:30:36 +0530

    Update hce-util.spec.js
    test

--- www/hce-util.spec.js
+++ www/hce-util.spec.js
@@ -1,45 +1,65 @@
-// Mocha Tests
+// hce-util.spec.js
 
-// Cordova HCE Plugin
-// (c) 2015 Don Coleman
+// NOTE: This file is for Node/Mocha testing only.
 
+// Do NOT include this file inside OutSystems mobile build.
+ 
 var assert = require('assert');
-var util = require('./hce-util');
 
+var util = require('./hce-util.js');
+ 
 describe('HCE Util', function () {
-
+ 
     it('should convert hex strings to byte arrays', function () {
+
         var test = util.hexStringToByteArray('656667');
+
         var expected = new Uint8Array([0x65, 0x66, 0x67]);
-        assert.equal(expected.toString(), test.toString());
-    });
 
+        assert.strictEqual(expected.toString(), test.toString());
+
+    });
+ 
     it('should convert byte arrays to hex strings', function () {
+
         var data = new Uint8Array([0x65, 0x66, 0x67]);
-        assert.equal('656667', util.byteArrayToHexString(data));
-    });
 
+        assert.strictEqual('656667', util.byteArrayToHexString(data));
+
+    });
+ 
     it('should convert strings to byte arrays', function () {
+
         var test = util.stringToBytes('hello');
+
         var expected = new Uint8Array([104, 101, 108, 108, 111]);
-        assert.equal(expected.toString(), test.toString());
+
+        assert.strictEqual(expected.toString(), test.toString());
 
     });
-
+ 
     it('should combine buffers', function () {
+
         var a = new Uint8Array([63, 64]);
+
         var b = new Uint8Array([65, 66]);
-
-        var expected = new Uint8Array([63,64,65,66]).toString();
-
+ 
+        var combinedExpected = new Uint8Array([63, 64, 65, 66]).toString();
+ 
         var test = util.concatenateBuffers(a, b);
-        assert.equal(expected, new Uint8Array(test).toString());
 
+        assert.strictEqual(combinedExpected, new Uint8Array(test).toString());
+ 
         var test2 = util.concatenateBuffers(a.buffer, b.buffer);
-        assert.equal(expected, new Uint8Array(test2).toString());
 
+        assert.strictEqual(combinedExpected, new Uint8Array(test2).toString());
+ 
         var test3 = util.concatenateBuffers(a.buffer, b);
-        assert.equal(expected, new Uint8Array(test2).toString());
-    });
 
+        assert.strictEqual(combinedExpected, new Uint8Array(test3).toString());
+
+    });
+ 
 });
+
+ 
